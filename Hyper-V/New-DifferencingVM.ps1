52639b53413255ca5125089f5cee17d8f2bf5799
Oddvar Moe <oddvar.moe@advania.no>
2017-01-17 19:03:24 +0100

    Added Hyper-v Section

---
+++ Hyper-V/New-DifferencingVM.ps1
@@ -0,0 +1,111 @@
+﻿function New-DifferencingVM
+<#
+.Synopsis
+   Function to create a Virtual machine based on a master vhd - aka differencing disk.
+   Author: Oddvar Moe
+   Required Dependencies: Hyper-v module
+.DESCRIPTION
+   Function to create a Virtual machine based on a master vhd - aka differencing disk
+.EXAMPLE
+   New-DifferencingVM -VMName Kundetest1 -VMLocation "D:\VirtualMachines" -VMNetwork EXT-Wireless -VMOS Windows10 -VMMemory 2048MB -VMDiskSize 60GB
+#>
+{
+    [CmdletBinding(DefaultParameterSetName="VMOS")] 
+    [Alias()]
+    [OutputType([int])]
+    Param
+    (
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true)]
+        $VMName,
+        
+        [Parameter(Mandatory=$true,ParameterSetName="VMOS")]
+        [ValidateSet("Windows10","Server2012R2")]
+        $VMOS,
+
+        [Parameter(Mandatory=$false)]
+        $VMLocation="D:\VirtualMachines",
+
+        [Parameter(Mandatory=$false,ParameterSetName="MasterVHDPath")]
+        $MasterVHD,
+        
+        #A valid format is 2048MB, Default is 2048MB
+        [Parameter(Mandatory=$false)]
+        $VMMemory=2048MB,
+    
+        #A valid format is 60GB
+        [Parameter(Mandatory=$false)]
+        $VMDiskSize=60GB
+    )
+    DynamicParam
+    {
+        # Sets the dynamic parameters name
+        $ParameterName = 'VMNetwork'
+ 
+        # Create a dictionary
+        $RuntimeParameterDictionary = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
+ 
+        # Create a collection of attributes
+        $AttributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+ 
+        # Create and set the parameters' attributes
+        $ParameterAttribute = New-Object System.Management.Automation.ParameterAttribute
+        $ParameterAttribute.ValueFromPipeline = $true
+        $ParameterAttribute.ValueFromPipelineByPropertyName = $true
+        $ParameterAttribute.Mandatory = $true
+ 
+        # Add the attributes to the attributes collection
+        $AttributeCollection.Add($ParameterAttribute)
+ 
+        # Generate and set the ValidateSet
+        $arrSet = (Get-VMSwitch).Name
+        $ValidateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute($arrSet)
+ 
+        # Add the ValidateSet to the attributes collection
+        $AttributeCollection.Add($ValidateSetAttribute)
+ 
+        # Create and return the dynamic parameter
+        $RuntimeParameter = New-Object System.Management.Automation.RuntimeDefinedParameter($ParameterName, [string], $AttributeCollection)
+        $RuntimeParameterDictionary.Add($ParameterName, $RuntimeParameter)
+        return $RuntimeParameterDictionary
+    }
+
+    Begin
+    {
+        #To bind the dynamic parameter to a variable
+        $VMNetwork = $PsBoundParameters[$ParameterName]
+    }
+    Process
+    {
+        if ($PSCmdlet.ParameterSetName -eq "VMOS") {
+            if($VMOS -eq "Windows10")
+            {
+                #Client
+                $MasterVHD = "D:\HYPERV-MasterImages\Win10Ent1607x64MasterDisk\Win10Ent1607x64MasterDisk.vhdx"
+            }
+    
+            if($VMOS -eq "Server2012R2")
+            {
+                $MasterVHD = "D:\HYPERV-MasterImages\Server2012R2\MDT-MasterServer\Virtual Hard Disks\MDT-MasterServer-Disk1.vhdx"
+            }
+        }
+    
+        try
+        {
+            New-VM -Name $VMName -MemoryStartupBytes $VMMemory -SwitchName $VMNetwork -Path $VMLocation -NoVHD -Verbose
+            New-VHD -ParentPath $MasterVHD -Differencing -Path "$VMLocation\$VMName\Virtual Hard Disks\$VMName-Disk1.vhdx" -SizeBytes $VMDiskSize -Verbose
+            Add-VMHardDiskDrive -VMName $VMName -Path "$VMLocation\$VMName\Virtual Hard Disks\$VMName-Disk1.vhdx" -Verbose
+        }
+        catch
+        {
+            Write-Host "Was not able to do my stuff"
+            Write-Error $_.Exception.Message
+        }
+        finally
+        {
+        }
+    }
+    End
+    {
+    }
+}
---
+++ Hyper-V/Remove-VirtualMachine.ps1
@@ -0,0 +1,83 @@
+﻿Function Remove-VirtualMachine
+<#
+.Synopsis
+   Function to remove Virtual machine and files. Gets VM names dynamically.
+   Author: Oddvar Moe
+   Required Dependencies: Hyper-v module
+.DESCRIPTION
+   Function to remove Virtual machine and files. Gets VM names dynamically.
+.EXAMPLE
+   PS C:\> Remove-VirtualMachine -VMName AAA -Verbose
+
+   Removes the virtual machine named AAA 
+#>
+{
+    [CmdletBinding()]
+    Param()
+    DynamicParam
+    {
+        # Sets the dynamic parameters name
+        $ParameterName = 'VMName'
+ 
+        # Create a dictionary
+        $RuntimeParameterDictionary = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
+ 
+        # Create a collection of attributes
+        $AttributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+ 
+        # Create and set the parameters' attributes
+        $ParameterAttribute = New-Object System.Management.Automation.ParameterAttribute
+        $ParameterAttribute.ValueFromPipeline = $true
+        $ParameterAttribute.ValueFromPipelineByPropertyName = $true
+        $ParameterAttribute.Mandatory = $true
+ 
+        # Add the attributes to the attributes collection
+        $AttributeCollection.Add($ParameterAttribute)
+ 
+        # Generate and set the ValidateSet
+        $arrSet = (Get-Vm).Name
+        $ValidateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute($arrSet)
+ 
+        # Add the ValidateSet to the attributes collection
+        $AttributeCollection.Add($ValidateSetAttribute)
+ 
+        # Create and return the dynamic parameter
+        $RuntimeParameter = New-Object System.Management.Automation.RuntimeDefinedParameter($ParameterName, [string], $AttributeCollection)
+        $RuntimeParameterDictionary.Add($ParameterName, $RuntimeParameter)
+        return $RuntimeParameterDictionary
+    }
+
+    Begin
+    {
+        #To bind the dynamic parameter to a variable
+        $VMName = $PsBoundParameters[$ParameterName]
+    }
+    Process
+    {
+        try
+        {
+            $VM = Get-VM -Name $VMName
+            $disks = Get-VHD -VMId $vm.Id
+            
+            Write-Verbose "Removing snapshots if any"
+            Remove-VMSnapshot -VMName $VMName –IncludeAllChildSnapshots
+            Write-Verbose "Removing virtual harddrive"
+            Remove-Item $disks.path -Force
+            Write-Verbose "Removing VM"
+            Remove-vm -Name $VMName -Force
+            Write-Verbose "Removing VM files and folders"
+            Remove-item -path $VM.path -Recurse -force
+        }
+        catch
+        {
+            Write-Host "Was not able to do my stuff"
+            Write-Error $_.Exception.Message
+        }
+        finally
+        {
+        }
+    }
+    End
+    {
+    }
+}
